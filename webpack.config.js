46f18b27220c5e89a4bb2cffe4d2a63bc497c7d6
darkdelirium <alucard.ui@gmail.com>
2019-03-21 16:46:46 -0700

    fixed css processing errors, added scss processing

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const config = {
 
   // Output
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
 
@@ -36,8 +36,31 @@ const config = {
       // CSS Files
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'less-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader']
+      },
+      {
+            test: /\.(png|jp(e*)g|svg|gif)$/,  
+            use: [{
+                loader: 'file-loader'
+            }]
+      },
+      {
+	test: /\.(scss)$/,
+	use: [{loader: 'style-loader', // inject CSS to page
+		}, {loader: 'css-loader', // translates CSS into CommonJS modules
+		}, {loader: 'postcss-loader', // Run post css actions
+	options: {
+        plugins: function () { // post css plugins, can be exported to postcss.config.js
+          return [
+            require('precss'),
+            require('autoprefixer')
+          ];
+        }
       }
+    }, {
+      loader: 'sass-loader' // compiles Sass to CSS
+    }]
+  }
     ]
   },
 
