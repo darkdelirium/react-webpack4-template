eaee5a39a1509ee3f7ea431b03d687bc715ffcf2
darkdelirium <alucard.ui@gmail.com>
2019-03-22 20:11:25 -0700

    fixed some css loading issues

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,76 @@
 */
 
 // Imports: Dependencies
-const path = require('path');
+const path = require("path");
 require("babel-register");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // Webpack Configuration
 const config = {
-  
   // Entry
-  entry: './src/index.js',
+  entry: "./src/index.js",
 
   // Output
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
   },
 
   // Loaders
   module: {
-    rules : [
+    rules: [
       // JavaScript/JSX Files
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"]
       },
       // CSS Files
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        loader: "style-loader!css-loader?modules"
       },
       {
-            test: /\.(png|jp(e*)g|svg|gif)$/,  
-            use: [{
-                loader: 'file-loader'
-            }]
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
       },
       {
-	test: /\.(scss)$/,
-	use: [{loader: 'style-loader', // inject CSS to page
-		}, {loader: 'css-loader', // translates CSS into CommonJS modules
-		}, {loader: 'postcss-loader', // Run post css actions
-	options: {
-        plugins: function () { // post css plugins, can be exported to postcss.config.js
-          return [
-            require('precss'),
-            require('autoprefixer')
-          ];
-        }
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: "style-loader" // inject CSS to page
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS modules
+          },
+          {
+            loader: "postcss-loader", // Run post css actions
+            options: {
+              plugins: function() {
+                // post css plugins, can be exported to postcss.config.js
+                return [require("precss"), require("autoprefixer")];
+              }
+            }
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
       }
-    }, {
-      loader: 'sass-loader' // compiles Sass to CSS
-    }]
-  }
     ]
   },
 
   // Plugins
-  plugins: [new HtmlWebpackPlugin({template: "./src/index.html"})],
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
 };
 
 // Exports
